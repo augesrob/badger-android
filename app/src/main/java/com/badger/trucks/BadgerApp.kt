0133a28e003bfb508ecc3229ff9b3db945614846
augesrob <augesrob@live.com>
2026-02-28 13:00:07 -0600

    Fix Duration type for reconnectDelay

--- app/src/main/java/com/badger/trucks/BadgerApp.kt
+++ app/src/main/java/com/badger/trucks/BadgerApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
+import kotlin.time.Duration.Companion.seconds
 
 class BadgerApp : Application() {
     companion object {
@@ -15,7 +16,7 @@ class BadgerApp : Application() {
                 install(Postgrest)
                 install(Realtime) {
                     secure = true
-                    reconnectDelay = 3000
+                    reconnectDelay = 3.seconds
                 }
             }
         }
